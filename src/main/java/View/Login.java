a95a2a134faee4db688b1699e12c41bee5303c59
Andrew <Andrew@AndrewPC>
2020-11-17 02:24:11 -0500

    Login.java upgrades, added function to clear buttons -- need to work on checking if login values are true and alignment to center of screen

--- src/main/java/View/Login.java
+++ src/main/java/View/Login.java
@@ -5,24 +5,33 @@ IST 261 Assignment:
 @author: Andrew Hallett
 date: 
  */
-
-
 package View;
+
 import java.io.FileNotFoundException;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Login extends Application {
+
+    Stage loginWindow;
+    Scene loginScene, sucessScene;
 
     @Override
     public void start(Stage loginStage) throws FileNotFoundException {
@@ -46,8 +55,10 @@ public class Login extends Application {
         //Creating Buttons
         Button enterButton = new Button("Enter");
         enterButton.setStyle("-fx-background-color: #008000");
-        Button clearButton = new Button("Clear");
-        clearButton.setStyle("-fx-background-color: #FF0000");
+        Button clearUserButton = new Button("Clear Username");
+        clearUserButton.setStyle("-fx-background-color: #FF0000");
+        Button clearPassButton = new Button("Clear Password");
+        clearPassButton.setStyle("-fx-background-color: #FF0000");
 
         //Creating a pane iwht a grid layout
         GridPane gridPane = new GridPane();
@@ -69,15 +80,55 @@ public class Login extends Application {
         gridPane.add(emailField, 1, 0);
         gridPane.add(passText, 0, 1);
         gridPane.add(passField, 1, 1);
-        gridPane.add(enterButton, 1, 2);
-        gridPane.add(clearButton, 0, 2);
+        gridPane.add(enterButton, 2, 4);
+        gridPane.add(clearUserButton, 1, 0, 2, 2);
+        gridPane.add(clearPassButton, 1, 1, 3, 3);
 
         //Styling nodes  
         enterButton.setFont(Font.font("Zapfino", FontWeight.BOLD, 25));
         //enterButton.setStyle("-fx-text-fill: black");
 
-        //Changing text
-        clearButton.setFont(Font.font("Zapfino", FontWeight.BOLD, 25));
+        //Changing text for the clear username button
+        clearUserButton.setFont(Font.font("Zapfino", FontWeight.BOLD, 12));
+        //Changing text for the clear password button
+        clearPassButton.setFont(Font.font("Zapfino", FontWeight.BOLD, 12));
+        //Adding event listener to the clear button to clear email field
+        clearUserButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                emailField.setText("");
+            }
+        });
+        //Adding event listener to the clear button to clear password field
+        clearPassButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                passField.setText("");
+            }
+        });
+
+        //Enter button does not work -- also need to add not bringing user to 
+        //the login scene when credentials are wrong
+        enterButton.setOnAction(e -> loginWindow.setScene(sucessScene));
+        //Creating layout for window that opens when enter is pressed
+        //Enter does not check for correct login values yet
+        VBox sucessBox = new VBox();
+        sucessBox.setPadding(new Insets(15));
+        sucessBox.setSpacing(10);
+
+        Text sucessText = new Text("Login Sucessful!");
+        sucessBox.getChildren().add(sucessText);
+        sucessText.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+        //Setting the layout and size for sucess scene login
+        sucessScene = new Scene(sucessBox, 600, 400);
+
+        //Changing the size of buttons
+        enterButton.setPrefWidth(500);
+        //Setting enter button to be in middle of scene
+        enterButton.setAlignment(Pos.CENTER);
+        //Changing size of both clear buttons
+        clearPassButton.setPrefWidth(110);
+        clearUserButton.setPrefWidth(110);
 
         //Changing text
         userText.setFont(Font.font("Zapfino", FontWeight.BOLD, 25));
@@ -87,6 +138,16 @@ public class Login extends Application {
         gridPane.setStyle("-fx-border-color: blue");
         gridPane.setStyle("-fx-background-color: GRAY;");
 
+        //Aligning everything in the middle of the screen 
+        //userText.setAlignment(Pos.CENTER);
+        //passText.setAlignment(Pos.CENTER);
+        //emailField.setAlignment(Pos.CENTER);
+        //passField.setAlignment(Pos.CENTER);
+        //enterButton.setAlignment(Pos.CENTER);
+        //clearUserButton.setAlignment(Pos.CENTER);
+        //userText.setTextAlignment(TextAlignment.CENTER);
+        //gridPane.add(userText, 0, 1);
+        //GridPane.setHalignment(userText, HPos.CENTER);
         //Creating a scene object 
         Scene scene = new Scene(gridPane);
 
@@ -101,10 +162,8 @@ public class Login extends Application {
 
     }
 
+    public static void main(String[] args) {
+        launch(args);
 
-       
-public static void main(String[] args) {
-    launch(args);
-   
-}
+    }
 }
