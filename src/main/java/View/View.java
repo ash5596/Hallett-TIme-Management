2e5449fd5a30d1a885b890b86db7a46955dfb8f9
Andrew <Andrew@AndrewPC>
2020-12-02 17:32:20 -0500

    sets and gets questions

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -1,20 +1,55 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Author: Andrew Hallett
+Class:
+Prof: Dr.Yeh
+Class: IST 261 App Dev Studio
+Description: 
  */
 package View;
+
+import javafx.stage.Stage;
 
 /**
  *
  * @author Andrew
  */
 public class View {
+    Stage MainStage;
+    Stage Login;
+    Stage ChooseDate;
+    Stage CreateAccount;
+            
     
-    ChooseDate chooseDate;
-    Login login;
-    MainStage mainStage;
-    
-    //Constructor
+    public View() {
+        MainStage = new Stage();
+        Login = MainStage.Login();
+        ChooseDate = MainStage.Login();
+        CreateAccount = MainStage.Login();
+        
+        
+                
+        
+    }
+
+    public Stage getMainStage() {
+        return MainStage;
+    }
+
+    public Stage getLogin() {
+        return Login;
+    }
+
+    public Stage getChooseDate() {
+        return ChooseDate;
+    }
+
+    public Stage getCreateAccount() {
+        return CreateAccount;
+    }
+ 
    
+    
+    
+    
+    
 }
