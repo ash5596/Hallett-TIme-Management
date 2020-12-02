2cb28f347199478b32b0447d3ffaa8486d2f1585
Andrew <Andrew@AndrewPC>
2020-12-02 17:32:38 -0500

    sets and gets questions

--- src/main/java/Model/Model.java
+++ src/main/java/Model/Model.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+File name: 
+Short description: 
+IST 261 Assignment: 
+@author: Andrew Hallett
+date: 
  */
 package Model;
 
@@ -12,12 +14,13 @@ package Model;
 public class Model {
     
     
+    public Model() {
+        
+    
 }
 
     
-    get getChooseDate {
-        return datePicker;
-}
+
             
 }
 //database
